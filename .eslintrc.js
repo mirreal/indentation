c79c3c0e3c1518fed39a73a17a690c9ba68f2e93
mirreal <zgkm181142521@gmail.com>
2018-02-09 18:28:09 +0800

    project init

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    env: {
+        node: true,
+        commonjs: true,
+        es6: true
+    },
+    globals: {
+    },
+    extends: [
+        'link-base'
+    ],
+    rules: {
+        // indent: [2, 2]
+    }
+}
